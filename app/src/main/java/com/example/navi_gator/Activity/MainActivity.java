8020bb4d74df0a4d4e0e950e5955636fd5cba8dc
Bart <ba.peereboom@gmail.com>
2019-12-15 21:00:03 +0100

    Added: New main screen
    (+): Main Activity with a simple title, picture and button. Picture not yet implemented. Has been changed to the launch activity.
    (+): String reference for the button text

--- app/src/main/java/com/example/navi_gator/Activity/MainActivity.java
+++ app/src/main/java/com/example/navi_gator/Activity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.navi_gator.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.navi_gator.R;
 
@@ -17,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btnStartMaps);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
     }
 }
